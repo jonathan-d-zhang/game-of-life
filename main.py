2969a1b4361717e69eba7b11615b2f81ca2aafec
SavagePastaMan <jonathanzhang929@gmail.com>
2021-05-25 11:14:02 -0400

    Implement Game of Life in python for testing

--- main.py
+++ main.py
@@ -4,15 +4,44 @@ import arcade
 SQR_LEN = 10
 SCREEN_LEN = 512
 
-squares = set()
-for _ in range(15):
-    x = randrange(-15, 15)
-    y = randrange(-15, 15)
-    squares.add((x, y))
+squares = {(0, 0), (0, 1), (0, 2)}
+
+
+def neighbors(tile, with_self=False):
+    if with_self:
+        yield tile
+
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 == dy:
+                continue
+            yield tile[0] + dx, tile[1] + dy
+
+
+def update():
+    seen = set()
+    flipped = set()
+    for tile in squares:
+        for neighbor in neighbors(tile, with_self=True):
+            if neighbor in seen:
+                continue
+            seen.add(neighbor)
+
+            s = sum(
+                neighbors_neighbor in squares
+                for neighbors_neighbor in neighbors(neighbor)
+            )
+            if (neighbor in squares and (s != 2 and s != 3)) or (
+                neighbor not in squares and s == 3
+            ):
+                flipped.add(neighbor)
+
+    squares.symmetric_difference_update(flipped)
 
 
 def draw(delta):
     arcade.start_render()
+    update()
     for x, y in squares:
         x *= SQR_LEN
         y *= SQR_LEN
@@ -21,10 +50,9 @@ def draw(delta):
         arcade.draw_lrtb_rectangle_filled(
             x, x + SQR_LEN, y + SQR_LEN, y, arcade.color.BLACK
         )
-    squares.add((randrange(-15, 15), randrange(-15, 15)))
 
 
 arcade.open_window(SCREEN_LEN, SCREEN_LEN, "test")
 arcade.set_background_color(arcade.color.WHITE)
-arcade.schedule(draw, 0.5)
+arcade.schedule(draw, 0.01)
 arcade.run()
