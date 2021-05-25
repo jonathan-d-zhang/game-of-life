c78fc63a4ef6ed6c1c943800d7a3b686d14f8780
SavagePastaMan <jonathanzhang929@gmail.com>
2021-05-25 17:11:42 -0400

    Add glider initial state

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from random import randrange
 import arcade
 
 SQR_LEN = 10
 SCREEN_LEN = 512
 
-squares = {(0, 0), (0, 1), (0, 2)}
+squares = {
+    (0, 0),
+    (1, 0),
+    (2, 0),
+    (2, 1),
+    (1, 2),
+}
 
 
 def neighbors(tile, with_self=False):
@@ -50,9 +55,12 @@ def draw(delta):
         arcade.draw_lrtb_rectangle_filled(
             x, x + SQR_LEN, y + SQR_LEN, y, arcade.color.BLACK
         )
+        arcade.draw_lrtb_rectangle_outline(
+            x, x + SQR_LEN, y + SQR_LEN, y, arcade.color.WHITE
+        )
 
 
 arcade.open_window(SCREEN_LEN, SCREEN_LEN, "test")
 arcade.set_background_color(arcade.color.WHITE)
-arcade.schedule(draw, 0.01)
+arcade.schedule(draw, 0.25)
 arcade.run()
