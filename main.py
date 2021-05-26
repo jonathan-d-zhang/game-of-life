e9960299a3f9e0cc2ee1d9cff6cb4d45de73a846
SavagePastaMan <jonathanzhang929@gmail.com>
2021-05-25 21:02:25 -0400

    Switch to rust impl in `main.py`
    Removed existing python implementation.

--- main.py
+++ main.py
@@ -1,57 +1,25 @@
 import arcade
+import game_of_life as gol
 
 SQR_LEN = 10
 SCREEN_LEN = 512
 
-squares = {
+squares = [
     (0, 0),
     (1, 0),
     (2, 0),
     (2, 1),
     (1, 2),
-}
+]
 
 
-def neighbors(tile, with_self=False):
-    if with_self:
-        yield tile
-
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            if dx == 0 == dy:
-                continue
-            yield tile[0] + dx, tile[1] + dy
-
-
-def update():
-    seen = set()
-    flipped = set()
-    for tile in squares:
-        for neighbor in neighbors(tile, with_self=True):
-            if neighbor in seen:
-                continue
-            seen.add(neighbor)
-
-            s = sum(
-                neighbors_neighbor in squares
-                for neighbors_neighbor in neighbors(neighbor)
-            )
-            if (neighbor in squares and (s != 2 and s != 3)) or (
-                neighbor not in squares and s == 3
-            ):
-                flipped.add(neighbor)
-
-    squares.symmetric_difference_update(flipped)
-
-
-def draw(delta):
+def draw(_):
+    global squares
     arcade.start_render()
-    update()
+    squares = gol.step(squares)
     for x, y in squares:
-        x *= SQR_LEN
-        y *= SQR_LEN
-        x += SCREEN_LEN // 2
-        y += SCREEN_LEN // 2
+        x = x * SQR_LEN + SCREEN_LEN // 2
+        y = y * SQR_LEN + SCREEN_LEN // 2
         arcade.draw_lrtb_rectangle_filled(
             x, x + SQR_LEN, y + SQR_LEN, y, arcade.color.BLACK
         )
