2a6c6548f0e9408f79c23bca55f601d4ec11695f
SavagePastaMan <jonathanzhang929@gmail.com>
2021-05-25 20:59:44 -0400

    Create game of life impl in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,61 @@
-use cpython::{PyResult, PySet};
 use std::collections::HashSet;
 
 pub mod py_interface;
 
-fn step(alive: HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
-    let mut res = HashSet::new();
+fn neighbors(point: (i8, i8), with_self: bool) -> Vec<(i8, i8)> {
+    let (x, y) = point;
+    let mut res = Vec::new();
+    if with_self {
+        res.push((x, y));
+    }
+
+    for dx in -1..2 {
+        for dy in -1..2 {
+            if dx == dy && dy == 0 {
+                continue;
+            }
+            res.push((x + dx, y + dy));
+        }
+    }
 
     res
+}
+
+fn step(alive: HashSet<(i8, i8)>) -> Vec<(i8, i8)> {
+    let mut seen = HashSet::new();
+    let mut flipping = HashSet::new();
+
+    for cell in alive.iter() {
+        for neighbor in neighbors(*cell, true) {
+            if seen.contains(&neighbor) {
+                continue;
+            }
+            seen.insert(neighbor);
+
+            let s = neighbors(neighbor, false)
+                .iter()
+                .filter(|c| alive.contains(c))
+                .count();
+
+            if alive.contains(&neighbor) && (s != 2 && s != 3)
+                || !alive.contains(&neighbor) && (s == 3)
+            {
+                flipping.insert(neighbor);
+            }
+        }
+    }
+
+    let mut temp = Vec::new();
+    for cell in alive.iter() {
+        if !flipping.contains(cell) {
+            temp.push(*cell);
+        }
+    }
+    for cell in flipping.iter() {
+        if !alive.contains(cell) {
+            temp.push(*cell);
+        }
+    }
+
+    temp
 }
