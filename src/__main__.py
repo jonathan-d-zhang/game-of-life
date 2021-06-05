e951fd2449676050394ed8910529cd605f1b3a48
EwwPhysics <sashakmurray@gmail.com>
2021-06-05 12:01:32 -0400

    Make graphics object oriented
    This will make it easier to add buttons and allow the user to interact with the window.

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,4 @@
+import time
 import arcade
 from . import game_of_life
 
@@ -22,36 +23,40 @@ squares[-1][0] = 1
 squares[-1][-1] = 1
 
 
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_LEN, SCREEN_LEN, "Conway's Game of Life")
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        global squares
+        arcade.start_render()
+        for y in range(LIST_LEN):
+            my = map_sc(y)
+            for x in range(LIST_LEN):
+                mx = map_sc(x)
+                if squares[y][x] == 1:
+                    arcade.draw_lrtb_rectangle_filled(
+                        mx,
+                        mx + SQR_LEN,
+                        my + SQR_LEN,
+                        my,
+                        arcade.color.BLACK,
+                    )
+                    arcade.draw_lrtb_rectangle_outline(
+                        mx,
+                        mx + SQR_LEN,
+                        my + SQR_LEN,
+                        my,
+                        arcade.color.WHITE,
+                    )
+        time.sleep(.1)
+        squares = game_of_life.step(squares)
+
+
 def map_sc(n: int) -> int:
     return (n - LIST_LEN // 2) * SQR_LEN + SCREEN_LEN // 2
 
 
-def draw(_):
-    global squares
-    arcade.start_render()
-    for y in range(LIST_LEN):
-        my = map_sc(y)
-        for x in range(LIST_LEN):
-            mx = map_sc(x)
-            if squares[y][x] == 1:
-                arcade.draw_lrtb_rectangle_filled(
-                    mx,
-                    mx + SQR_LEN,
-                    my + SQR_LEN,
-                    my,
-                    arcade.color.BLACK,
-                )
-                arcade.draw_lrtb_rectangle_outline(
-                    mx,
-                    mx + SQR_LEN,
-                    my + SQR_LEN,
-                    my,
-                    arcade.color.WHITE,
-                )
-    squares = game_of_life.step(squares)
-
-
-arcade.open_window(SCREEN_LEN, SCREEN_LEN, "test")
-arcade.set_background_color(arcade.color.WHITE)
-arcade.schedule(draw, 0.1)
+window = Game()
 arcade.run()
