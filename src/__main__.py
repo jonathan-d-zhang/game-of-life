f8c1b221c52cdbbac20247df24e380342b1a0983
EwwPhysics <sashakmurray@gmail.com>
2021-06-07 00:06:35 -0400

    Add pausing and editing functionality
    Users can pause the game at any time by pressing the pause button, and resume by pressing it again.
    While the game is paused, users can change which cells are alive by clicking on them.

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,10 @@
 import time
 import arcade
 from . import game_of_life
-from constants import *
+from .constants import *
 
 
 class Game(arcade.Window):
-
     def __init__(self):
         super().__init__(SCREEN_LEN, SCREEN_LEN, "Conway's Game of Life")
         arcade.set_background_color(arcade.color.WHITE)
@@ -17,17 +16,34 @@ class Game(arcade.Window):
         self.squares[10][12] = 1
         self.squares[11][12] = 1
         self.squares[12][11] = 1
-        
+
         # block
         self.squares[0][0] = 1
         self.squares[0][-1] = 1
         self.squares[-1][0] = 1
         self.squares[-1][-1] = 1
-        self.paused: bool = True
 
+        self.pause_button = arcade.Sprite(
+            "src/ui/play.png",
+            scale=0.08,
+            center_x=SCREEN_LEN - 60,
+            center_y=SCREEN_LEN - 35,
+        )
+
+        self.edit_button = arcade.Sprite(
+            "src/ui/edit.png",
+            scale=0.05,
+            center_x=SCREEN_LEN - 120,
+            center_y=SCREEN_LEN - 35,
+        )
+
+        self.paused: bool = False
+        self.editing: bool = False
 
     def on_draw(self):
         arcade.start_render()
+        self.pause_button.draw()
+        self.edit_button.draw()
         for y in range(LIST_LEN):
             my = map_sc(y)
             for x in range(LIST_LEN):
@@ -42,7 +58,26 @@ class Game(arcade.Window):
                     )
         if not self.paused:
             self.squares = game_of_life.step(self.squares)
-        time.sleep(.1)
+        time.sleep(0.1)
+
+    def on_mouse_press(self, x, y, _button, _modifiers):
+        if (
+            self.pause_button.left <= x <= self.pause_button.right
+            and self.pause_button.bottom <= y <= self.pause_button.top
+        ):
+            self.paused = not self.paused
+            if not self.paused:
+                self.editing = False
+        elif (
+            self.edit_button.left <= x <= self.edit_button.right
+            and self.edit_button.bottom <= y <= self.edit_button.top
+        ):
+            if self.paused:
+                self.editing = True
+        elif self.editing:
+            i = y // 16
+            j = x // 16
+            self.squares[i][j] = not self.squares[i][j]
 
 
 def map_sc(n: int) -> int:
@@ -51,4 +86,3 @@ def map_sc(n: int) -> int:
 
 window = Game()
 arcade.run()
-
