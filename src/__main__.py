879b80b106866016fe84e8a2687ae6cae373b079
EwwPhysics <sashakmurray@gmail.com>
2021-06-06 22:45:04 -0400

    Move constants to constants.py
    This will allow the constants to be imported to other files without circular import issues

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,7 @@
 import time
 import arcade
 from . import game_of_life
-
-SQR_LEN = 16
-SCREEN_LEN = 512
-
-LIST_LEN = SCREEN_LEN // SQR_LEN
-EDGE = SQR_LEN // 8
+from constants import *
 
 
 class Game(arcade.Window):
