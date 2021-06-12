bc8360c69490da03c1bbffa492cc22212f365e19
SavagePastaMan <jonathanzhang929@gmail.com>
2021-06-12 00:28:58 -0400

    Optimize `step` function
    New implementation is approximately 33% faster.

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,44 @@
 pub mod py_interface;
 
-/*
- *
-const fn generate_lookup(alive: bool) -> [u8; u8::MAX as usize] {
-    let mut lookup = [0; u8::MAX as usize];
-    let mut i: u8 = 0;
-    while i < u8::MAX {
-        let ones = i.count_ones();
-        if (alive && ones == 2) || ones == 3 {
+const fn generate_lookup() -> [u32; 512] {
+    let mut lookup = [0; 512];
+    let mut i: u32 = 0;
+    while i < 512 {
+        let center_square = (i >> 4) & 1;
+        let ones = i.count_ones() - center_square;
+        if center_square == 1 && (ones == 2 || ones == 3) || center_square == 0 && ones == 3 {
             lookup[i as usize] = 1;
         }
-
-        i += 1
+        i += 1;
     }
-
     lookup
 }
 
-const DEAD_LOOKUP: [u8; u8::MAX as usize] = generate_lookup(false);
-const ALIVE_LOOKUP: [u8; u8::MAX as usize] = generate_lookup(true);
-*/
+const LOOKUP: [u32; 512] = generate_lookup();
 
-const NEIGHBORS: [(i32, i32); 8] = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-];
 
-fn step(alive: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+fn step(alive: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
     let mut res = vec![vec![0; alive[0].len()]; alive.len()];
 
     let y_len = res.len();
     let x_len = res[0].len();
 
     for y in 0..y_len {
-        for x in 0..x_len {
-            let mut c = 0;
+        let y_left_wrap = match y {0 => y_len - 1, _ => y - 1};
+        let y_right_wrap = (y + 1) % y_len;
+        let mut c =
+            (alive[y_left_wrap][0] << 5) + (alive[y_left_wrap][1] << 4)
+            + (alive[y][0] << 3) + (alive[y][1] << 2)
+            + (alive[y_right_wrap][0] << 1) + alive[y_right_wrap][1];
 
-            for &(dx, dy) in NEIGHBORS.iter() {
-                let ny = (y as i32 + dy).rem_euclid(y_len as i32) as usize;
-                let nx = (x as i32 + dx).rem_euclid(x_len as i32) as usize;
-                if alive[ny][nx] == 1 {
-                    c += 1;
-                }
-            }
-            let prev_alive = alive[y][x] == 1;
-            if (prev_alive && (c == 2 || c == 3)) || (!prev_alive && c == 3) {
-                res[y][x] = 1;
-            }
+        for x in 0..=x_len {
+            let x_wrap = (x + 1) % x_len;
+            c = ((c % 64) << 3)
+                + (alive[y_left_wrap][x_wrap] << 2)
+                + (alive[y][x_wrap] << 1)
+                + alive[y_right_wrap][x_wrap];
+
+            res[y][x % x_len] = LOOKUP[c as usize];
         }
     }
 
--- src/py_interface.rs
+++ src/py_interface.rs
@@ -2,11 +2,11 @@ use cpython::PyResult;
 use cpython::{py_fn, py_module_initializer, Python};
 
 py_module_initializer!(game_of_life, |py, m| {
-    m.add(py, "step", py_fn!(py, step(points: Vec<Vec<u8>>)))?;
+    m.add(py, "step", py_fn!(py, step(points: Vec<Vec<u32>>)))?;
     Ok(())
 });
 
-fn step(_: Python, points: Vec<Vec<u8>>) -> PyResult<Vec<Vec<u8>>> {
+fn step(_: Python, points: Vec<Vec<u32>>) -> PyResult<Vec<Vec<u32>>> {
     let res = super::step(points);
     Ok(res)
 }
